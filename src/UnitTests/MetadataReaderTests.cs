e7d0debf17b7044479780f58705e0387753d90a3
JW <git@jann.bayern>
2017-04-19 22:50:00 +0200

    Test Types outsourced

--- src/UnitTests/MetadataReaderTests.cs
+++ src/UnitTests/MetadataReaderTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using QuickIEnumerableToExcelExporter;
 using Xunit;
 
@@ -76,37 +75,6 @@ namespace UnitTests
 
             Assert.Equal("#,#0", idProperty.Format);
             Assert.Null(nameProperty.Format);
-        }
-
-        private class TestExportItem
-        {
-            public string Name { get; set; }
-
-            [ExportToExcel(Header = "Identity", StringFormat = "#,#0")]
-            public int Id { get; set; }
-
-            [ExportToExcel(DisplayMemberPath = "Year")]
-            public DateTime Timestamp { get; set; }
-
-            [ExportToExcel(DisplayMemberPath = "Inner.Value")]
-            public NestedItem Nested { get; set; }
-        }
-
-        private class NestedItem
-        {
-            public string Value { get; set; }
-
-            public NestedItem Inner { get; set; }
-        }
-
-        private class TestExportItemWithIgnore
-        {
-            public int Id { get; set; }
-
-            public string Name { get; set; }
-
-            [ExportToExcel(Ignore = true)]
-            public string Password { get; set; }
         }
     }
 }
---
+++ src/UnitTests/TestTypes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using QuickIEnumerableToExcelExporter;
+
+namespace UnitTests
+{
+    internal class TestExportItem
+    {
+        public string Name { get; set; }
+
+        [ExportToExcel(Header = "Identity", StringFormat = "#,#0")]
+        public int Id { get; set; }
+
+        [ExportToExcel(DisplayMemberPath = "Year")]
+        public DateTime Timestamp { get; set; }
+
+        [ExportToExcel(DisplayMemberPath = "Inner.Value")]
+        public NestedItem Nested { get; set; }
+    }
+
+    internal class NestedItem
+    {
+        public string Value { get; set; }
+
+        public NestedItem Inner { get; set; }
+    }
+
+    internal class TestExportItemWithIgnore
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [ExportToExcel(Ignore = true)]
+        public string Password { get; set; }
+    }
+}
